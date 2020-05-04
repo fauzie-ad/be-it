87a8ba27825b75563cff78d509f077c15449a660
Fauzie Akhmad Diawan <64467349+fauzie-ad@users.noreply.github.com>
2020-05-05 01:45:30 +0700

    config awal aja

---
+++ application/controllers/HomeController.php
@@ -0,0 +1,8 @@
+<?php 
+class HomeController extends CI_Controller {
+    
+    public function index() {
+        echo "berhasil nongol di Browser hehehehe ALHAMDULILLAH";
+    }
+}
+?>
---
+++ application/models/Model_pengguna.php
@@ -0,0 +1,4 @@
+<?php 
+class Model_pengguna extends CI_Model {
+    
+}
